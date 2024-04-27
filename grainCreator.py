9419c92bbeb6657c67d57e392829aa00214595f4
Cody <nytemairqt@gmail.com>
2024-04-27 14:11:02 +1000

    added grain controls to interface

--- grainCreator.py
+++ grainCreator.py
@@ -42,19 +42,16 @@ def _convert_matrix_to_pixel_buffer(buffer):
 	return buffer		
 
 ''
-def _filter_gaussian(l=3, sig=1.0):
-	ax = np.linspace(-(l-1) / 2.0, (l-1) /2.0, l)
+def _filter_gaussian(k=3, sig=1.0):
+	ax = np.linspace(-(k-1) / 2.0, (k-1) /2.0, k)
 	gaussian = np.exp(-0.5 * np.square(ax) / np.square(sig))
 	kernel = np.outer(gaussian, gaussian)
 	return kernel / np.sum(kernel)
 
-def GRAINCREATOR_FN_generateGrain(name):
+def GRAINCREATOR_FN_generateGrain(name, clip_min=.4, clip_max=.7, k=3, sigma=1.0):
 	w = bpy.data.scenes[0].render.resolution_x
 	h = bpy.data.scenes[0].render.resolution_y	
 	buffer_size = (4 * w * h)
-
-	clip_min = .4
-	clip_max = .7
 
 	# KEEP ME
 	img = bpy.data.images.new(name=name, width=w, height=h)
@@ -75,10 +72,16 @@ def GRAINCREATOR_FN_generateGrain(name):
 	else:
 		pixels_to_paint[:, :, 0:3] = r[:, :, 0:1]	
 
+	print('==================================================')
+	print('==================================================')
+	print(clip_min, clip_max)
+	print('==================================================')
+	print('==================================================')
+
 	pixels_to_paint = np.clip(pixels_to_paint, clip_min, clip_max)
 
 	# Apply Gaussian Blur
-	kernel = _filter_gaussian()
+	kernel = _filter_gaussian(k=k, sig=sigma)
 	blur_result = np.convolve(pixels_to_paint.flatten(), kernel.flatten())
 
 	pixels = blur_result[:buffer_size].reshape(pixels_to_paint.shape)
@@ -117,6 +120,10 @@ class GRAINCREATOR_OT_generateGrain(bpy.types.Operator):
 	bl_options = {"REGISTER", "UNDO"}
 	bl_description = "Generates custom film grain image or sequence."
 
+	clip_min: bpy.props.FloatProperty(name='clip_min', default=.4)
+	clip_max: bpy.props.FloatProperty(name='clip_max', default=.7)
+	kernel_size: bpy.props.IntProperty(name='kernel_size', default=3)
+	sigma: bpy.props.FloatProperty(name='sigma', default=1.0)
 	start: bpy.props.IntProperty(name='start', default=1)
 	end: bpy.props.IntProperty(name='end', default=1)
 	sequence: bpy.props.BoolProperty(name='sequence', default=True)
@@ -132,10 +139,16 @@ class GRAINCREATOR_OT_generateGrain(bpy.types.Operator):
 		seq = []
 
 		for i in range(sequence_length):
-			grain = GRAINCREATOR_FN_generateGrain(name=f"grain_{i+1}")
+			grain = GRAINCREATOR_FN_generateGrain(
+				name=f"grain_{i+1}", 
+				clip_min=self.clip_min, 
+				clip_max=self.clip_max, 
+				k=self.kernel_size, 
+				sigma=self.sigma)
 			if sequence_length > 1:
 				seq.append(grain)
 
+		'''
 		scene = bpy.context.scene 
 		compositor_node_tree = scene.node_tree
 		image_node = compositor_node_tree.nodes.new(type="CompositorNodeImage")
@@ -147,6 +160,7 @@ class GRAINCREATOR_OT_generateGrain(bpy.types.Operator):
 			# when changing to SEQUENCE, blender is unable to "load" the image	
 			#image_node.image.source = 'SEQUENCE'
 			#image_node.frame_duration = len(seq)
+		'''
 			
 		return {'FINISHED'}
 
@@ -193,6 +207,13 @@ class GRAINCREATOR_PT_panelMain(bpy.types.Panel):
 		scene = context.scene
 
 		# Grain Settings
+		row = layout.row()
+		# need to set min/max's for these so people don't break shit
+		row.prop(context.scene, 'GRAINCREATOR_VAR_clip_min', text='Clip Min')
+		row.prop(context.scene, 'GRAINCREATOR_VAR_clip_max', text='Clip Max')
+		row = layout.row()
+		row.prop(context.scene, 'GRAINCREATOR_VAR_kernel', text='Kernel')
+		row.prop(context.scene, 'GRAINCREATOR_VAR_sigma', text='Sigma')
 
 		# Start & End Frames
 		row = layout.row()
@@ -202,18 +223,21 @@ class GRAINCREATOR_PT_panelMain(bpy.types.Panel):
 		# Create Grain
 		row = layout.row()
 		button_create_grain = row.operator(GRAINCREATOR_OT_generateGrain.bl_idname, text="Create Grain", icon="FILE_IMAGE")
-
+	
 		# Save Grain 
 		row = layout.row()
 		button_export_grain = row.operator(GRAINCREATOR_OT_generateGrain.bl_idname, text="Export Grain", icon_value=727)
 
-		
-
-		button_create_grain.start = context.scene.GRAINCREATOR_VAR_start
-		button_create_grain.end = context.scene.GRAINCREATOR_VAR_end
-
 		row = layout.row()
 		button_purge = row.operator(GRAINCREATOR_OT_clearUnused.bl_idname, text="purge(TEMP)", icon_value=727)
+
+		# Assign Variables
+		button_create_grain.clip_min = context.scene.GRAINCREATOR_VAR_clip_min
+		button_create_grain.clip_max = context.scene.GRAINCREATOR_VAR_clip_max
+		button_create_grain.kernel_size = context.scene.GRAINCREATOR_VAR_kernel
+		button_create_grain.sigma = context.scene.GRAINCREATOR_VAR_sigma		
+		button_create_grain.start = context.scene.GRAINCREATOR_VAR_start
+		button_create_grain.end = context.scene.GRAINCREATOR_VAR_end
 
 		
 
@@ -226,6 +250,10 @@ class GRAINCREATOR_PT_panelMain(bpy.types.Panel):
 classes_interface = (GRAINCREATOR_PT_panelMain,)
 classes_functionality = (GRAINCREATOR_OT_generateGrain, GRAINCREATOR_OT_createNodeGroup, GRAINCREATOR_OT_clearUnused)
 
+bpy.types.Scene.GRAINCREATOR_VAR_clip_min = bpy.props.FloatProperty(name='GRAINCREATOR_VAR_clip_min', default=.4, soft_min=0.0, soft_max=1.0, description='Squash Black Values in Generated Grain.')
+bpy.types.Scene.GRAINCREATOR_VAR_clip_max = bpy.props.FloatProperty(name='GRAINCREATOR_VAR_clip_max', default=.7, soft_min=0.0, soft_max=1.0, description='Squash White Values in Generated Grain.')
+bpy.types.Scene.GRAINCREATOR_VAR_kernel = bpy.props.IntProperty(name='GRAINCREATOR_VAR_kernel', default=3, soft_min=1, soft_max=16, description='Set Kernel Size for Gaussian Blur.')
+bpy.types.Scene.GRAINCREATOR_VAR_sigma = bpy.props.FloatProperty(name='GRAINCREATOR_VAR_sigma', default=1.0, soft_min=0.0, soft_max=5.0, description='Set Sigma for Gaussian Blur.')
 bpy.types.Scene.GRAINCREATOR_VAR_start = bpy.props.IntProperty(name='GRAINCREATOR_VAR_start', default=1, soft_min=0, description='Frame start for Grain generation.')
 bpy.types.Scene.GRAINCREATOR_VAR_end = bpy.props.IntProperty(name='GRAINCREATOR_VAR_end', default=1, soft_min=0, description='Frame end for Grain generation.')
 
@@ -244,6 +272,11 @@ def unregister():
 		bpy.utils.unregister_class(c)
 	for c in reversed(classes_functionality):
 		bpy.utils.unregister_class(c)
+
+	del bpy.types.Scene.GRAINCREATOR_VAR_clip_min
+	del bpy.types.Scene.GRAINCREATOR_VAR_clip_max
+	del bpy.types.Scene.GRAINCREATOR_VAR_kernel
+	del bpy.types.Scene.GRAINCREATOR_VAR_sigma		
 
 	del bpy.types.Scene.GRAINCREATOR_VAR_start
 	del bpy.types.Scene.GRAINCREATOR_VAR_end
